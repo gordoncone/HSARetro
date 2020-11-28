e2520e9d4f2db25faf5a7504ab9f84afbdeb88a0
Gordon <gordoncone@gmail.com>
2020-11-28 08:24:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+let hsaTeam = ["Barrett", "Matt", "Jody", "Lucian", "Ron", "Ann", "Dan", "Gordon", "Miguel"];
+const bCall = document.getElementById('buttonCall');
+let counter; 
+let ranRetro = [];
+
+
+function randomNumGen(){
+  // var counter = Math.floor((Math.random(hsaTeam) * 8) +0); 
+  var counter = Math.floor(Math.random() * hsaTeam.length); 
+  console.log(counter)
+  return counter
+};
+
+function retro(){
+  if(hsaTeam.length === 0){
+    hsaTeam = ranRetro;
+    ranRetro = [];
+    var guessWho = randomNumGen();
+    document.getElementById("h3").innerHTML = hsaTeam[guessWho] + " You're Up!" ;
+    document.getElementById('buttonCall').style.display='none'
+    ranRetro.push(hsaTeam[guessWho]);
+    hsaTeam.splice(guessWho, 1);
+    console.log("I'm equal to 0 therefore I reset hsaTeam");
+    console.log(hsaTeam);
+    console.log(ranRetro);
+  } else {
+    var guessWho = randomNumGen();
+    document.getElementById("h3").innerHTML = hsaTeam[guessWho] + " You're Up!" ;
+    document.getElementById('buttonCall').style.display='none'
+    ranRetro.push(hsaTeam[guessWho]);
+    hsaTeam.splice(guessWho, 1);
+    console.log("I'm not there yet, keep rolling");
+    console.log(hsaTeam);
+    console.log(ranRetro);
+  }
+}
+
+bCall.addEventListener('click', function(){
+    retro();
+    console.log("yes this works");
+});
+
+
+document.getElementById("reset").addEventListener('click', function(){
+  counter;
+  document.getElementById("h3").innerHTML = "[==================]" ;
+  document.getElementById('buttonCall').style.display='inline-block	'
+})
+
+//testings
